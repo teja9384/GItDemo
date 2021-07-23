ab31c5bafa294c63d39aae1e02fe5beab050cdfb
teja9384 <58102587+teja9384@users.noreply.github.com>
2021-07-23 09:21:31 +0530

    Update Tester.java
    Add script.

--- src/main/java/com/practice/main/Tester.java
+++ src/main/java/com/practice/main/Tester.java
@@ -6,5 +6,6 @@ public static void main(String[] args) {
 	System.out.println("Hello Tactics");
 	System.out.println("Hello People");
 	System.out.println("Hello Tactics");
+	System.out.println("Tactical Drive");
 }
 }
